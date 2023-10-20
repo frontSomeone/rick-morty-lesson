5a0315d2eeb7a4fbba8297344840fb7c74323ce9
Lenar Fattakhov <flaprogrammer@gmail.com>
2023-10-20 17:54:18 +0200

    Воркшоп. Добавляем Storybook

--- src/components/presentational/EpisodeList.jsx
+++ src/components/presentational/EpisodeList.jsx
@@ -1,10 +1,25 @@
 import { EpisodeItem } from "./EpisodeItem";
+import { Collapse } from "./Collapse";
+import { CharactersContainer } from "../containers/CharactersContainer";
 
 export const EpisodeList = ({ episodes }) => {
   return (
     <div>
       {episodes.map((episode) => (
-        <EpisodeItem key={episode.id} episode={episode} />
+        // <EpisodeItem key={episode.id} episode={episode} />
+        <Collapse
+          key={episode.id}
+          className="episode"
+          title={episode.episode + ":" + episode.name}
+          content={
+            <CharactersContainer
+              ids={episode.characters.map((character) => {
+                const id = character.split("/").pop();
+                return id;
+              })}
+            />
+          }
+        />
       ))}
     </div>
   );
